636798859ed25fb996682b0233ae687504cbc61a
Elisson Rocha <elissonrochaa@gmail.com>
2024-08-12 10:24:23 -0300

    add biblioteca

---
+++ OO/Biblioteca/livro.py
@@ -0,0 +1,43 @@
+class Livro:
+    #Meu Construtor
+    def __init__(self, titulo, autor, ano, editora="Não informado"):
+        #Atributos
+        #Os atributos obrigatorios precisam tá nos
+        #parametros sem valor predefinido, como é o caso do
+        #titulo, autor e ano.
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+        #O atributo opcional precisa ta nos parametros com um
+        #valor predefinido, como é o caso da editora. 
+        self.editora = editora
+        #O atributo que sempre será o mesmo valor, não se coloca
+        #nos parametros, como é o caso do disponivel
+        self.disponivel = True
+
+    def exibir_infos(self):
+        print("-"*30)
+        print("Livro:")
+        print(f"Titulo: {self.titulo}")
+        print(f"Autor: {self.autor}")
+        print(f"ano: {self.ano}")
+        print(f"editora: {self.editora}")
+        if self.disponivel:
+            print("Livro disponivel para emprestimo")
+        else:
+            print("Livro não disponivel para emprestimo")
+        print("-"*30)
+    
+    def emprestar(self):
+        if self.disponivel:
+            self.disponivel = False
+            return True
+        else:
+            return False
+    
+    def devolver(self):
+        if self.disponivel == False:
+            self.disponivel = True
+            return True
+        else:
+            return False
---
+++ OO/Biblioteca/main.py
@@ -0,0 +1,35 @@
+from livro import Livro
+
+def pegar_emprestado(livro):
+    print("Pegando o livro emprestrado")
+    foi_emprestado = livro.emprestar()
+    if foi_emprestado:
+        print("emprestimo realizado com sucesso")
+    else:
+        print("Emprestimo não realizado, livro não disponivel")
+
+def main():
+    #Cadastrando um Livro
+    meu_livro = Livro(titulo="Antes que o café esfrie",
+                      autor="Toshikazu Kawaguchi", ano="2022", 
+                      editora="Valentina")
+    
+    meu_livro3 = Livro(ano=2024, titulo="Antes que o café esfrie 3", 
+                       autor="Toshikazu Kawaguchi")
+    meu_livro2 = Livro("Antes que o café esfrie 2",
+                      "Toshikazu Kawaguchi", 2023, "Valentina")
+
+    meu_livro.exibir_infos()
+
+    #Pegando emprestado
+    pegar_emprestado(meu_livro)
+    #meu_livro.exibir_infos()
+
+    #Pegando emprestado
+    pegar_emprestado(meu_livro)
+
+    #Devolvendo
+    meu_livro.devolver()
+    meu_livro.exibir_infos()
+
+main()
